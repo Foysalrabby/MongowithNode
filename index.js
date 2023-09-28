34d98844e9b81f4ca8edf4c7a0cd73eebc89a8ea
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2023-09-29 02:33:34 +0600

    update not loadded

--- index.js
+++ index.js
@@ -1,57 +1,11 @@
-// const express = require('express');
-// const app = express();
-// const {MongoClient,ServerApiVersion} = require('mongodb');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const password = 'BfYFTrHtjFZ79ip7';
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cors());
-
-
-// const uri = "mongodb+srv://rabbi443418:BfYFTrHtjFZ79ip7@testermongo.fea0flo.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { 
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// },
-//   });
-
-
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
-
-
-
-// client.connect(err => {
-//   const collection = client.db("organicdata").collection("product");
-//   // perform actions on the collection object
-//   app.post('/addproduct', function (req, res) {
-//     const productitem = req.body;
-//     collection.insertOne(productitem)
-//     .then(result => {
-//       console.log("One product added");
-//   });
-        
-// });
-  
-//   console.log("database connected");
-
-// });
-
-
-// app.listen(3000);
 
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongodb=require('mongodb'); //to work delete
+  
 
 const uri = "mongodb+srv://rabbi443418:BfYFTrHtjFZ79ip7@testermongo.fea0flo.mongodb.net/?retryWrites=true&w=majority";
 
@@ -89,19 +43,103 @@ async function run() {
             }
         });
 
+
+
+
         // Insert to MongoDB
         app.post('/addproduct', (req, res) => {
             const productitem = req.body;
             col1.insertOne(productitem)
                 .then(result => {
                     console.log("Product added");
-                    res.send("Product added successfully");
+                    // res.send("Product added successfully");
+                    res.redirect('/');
                 })
                 .catch(err => {
                     console.error("Error adding product:", err);
                     res.status(500).send("Internal server error");
                 });
         });
+
+  //update read  data
+  app.get('/updatedata/:id', async(req,res)=>{
+
+    try{
+
+        const docunments=await col1.find({ _id :new mongodb.ObjectId(req.params.id)}).toArray();
+        res.send(docunments[0]);
+    }
+    catch(err){
+        console.error("Error Updating product:",err);
+        res.status(500).send("Internal server error");
+    }
+   })
+
+   //update to push data
+   app.patch('/updatedata/:id',async(req,res)=>{
+    try{
+        await col1.updateOne({ _id :new mongodb.ObjectId(req.params.id)},
+          {
+            $set: {
+                pdprice:req.body.price,quantity:req.body.quantityes
+              },
+              $currentDate: { lastUpdated: true }
+              
+
+          })
+         
+        //   res.send($currentDate > 0);
+
+    }
+    catch(err){
+        console.error("Error update push products:", err);
+        res.status(500).send("Internal server error");
+    }
+
+
+   })
+
+
+  //delete data
+  app.delete('/deletedata/:id',async (req, res)=>{
+
+    try{
+     
+         const result = await col1.deleteOne({ _id :new mongodb.ObjectId(req.params.id)});
+         console.log("Number of documents deleted: " + result.deletedCount);
+         res.send(result.deletedCount>0);
+        //console.log(req.params.id); 
+    }catch (err) {
+        console.error("Error reading products:", err);
+        res.status(500).send("Internal server error");
+    }
+
+
+  })
+
+// app.delete('/deletedata/:id', (req, res)=>{
+
+//       col1.deleteOne({_id:objectId(req.params.id)})
+//       .then(result =>{
+//         console.log("Product delete successfully");
+//       })
+//       .catch(err => {
+//         console.error("Error adding product:", err);
+//         res.status(500).send("Internal server error");
+//     });
+     
+      
+      //console.log("Number of documents deleted: " + result.deletedCount);
+        //console.log(req.params.id);  
+
+  //})
+
+
+
+
+
+
+
     } finally {
         // Close the MongoDB client when it's no longer needed
         // await client.close();
